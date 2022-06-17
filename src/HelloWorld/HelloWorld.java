c998d807414e7febb8374b7f4b45b6325085de78
Michael Wang <41721295+zwang20@users.noreply.github.com>
2022-06-18 04:20:59 +1000

    Minor changes

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -1,7 +1,7 @@
 package HelloWorld;
 
 public class HelloWorld {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
